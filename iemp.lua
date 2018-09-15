532320db462d3ee43d47c2e63f7896cbc34e8f4b
miscellaniuz <43073620+miscellaniuz@users.noreply.github.com>
2018-09-15 19:50:11 +0200

    issue #8 update

--- iemp.lua
+++ iemp.lua
@@ -2,36 +2,40 @@
 --
 -- Intelligent Energy Management Program (IEMP)
 -- 
--- Version: 0.2a
+-- Version: 0.3c
 --
 -- Author: Miscellaniuz
 --
 -- ////////////////////////
- 
+
 -- @ version //
-version = "0.2a"
-   
--- Startup
-print("Welcome to IEMP.")
-os.sleep(0.5)
-print("We are running on version " .. version)
-print("------------------------------------")
-print("Initializing.")
-os.sleep(1.5)
- 
- 
+version = "0.3c" 
+
+-- ToTheCore Utils import //
+os.loadAPI("/Utils/Utils")
+
 -- Variables //
 tickcount = 2 --Update interval of monitor in sec
 MinRF = 140000000 -- Minimum amount of RF storage before triggering energy backup system
- 
-print ("Update interval is set to " .. tickcount .. " seconds.")
-print ("Minimum trigger is set to " .. MinRF .. " RF.")
- 
+
+-- Startup
+Console.Init()
+Console.ClearScreen()
+Console.WriteLine(Console.Type.Info, "Welcome to IEMP.", m)
+Console.WriteLine(Console.Type.Line, "", m)
+Console.WriteLine(Console.Type.Info, "We are running on version " .. version, m)
+Console.WriteLine(Console.Type.Line, "", m)
+Console.WriteLine(Console.Type.Init, "UtilsAPI loaded.", m)
+Console.WriteLine(Console.Type.Config, "Update interval is set to " .. tickcount, m)
+Console.WriteLine(Console.Type.Config, "Minimum trigger is set to " .. MinRF, m)
+
+
 -- init //
 local cb = peripheral.wrap("capacitor_bank_1")
+Console.WriteLine(Console.Type.Init, "Capacitor Bank 1 found.", m)
 local monitor = peripheral.wrap("top")
+Console.WriteLine(Console.Type.Init, "Monitor found on top.", m)
 monitor.clear()
-monitor.setTextScale(0.5)
 monitor.setTextColor(colors.green)
 -- monitor.setBackgroundColor(255) --deactivated for later
 monitor.setCursorPos(1,1)
@@ -39,46 +43,42 @@ monitor.write("Intelligent Energy Management Program [IEMP] ver " .. version)
 monitor.setTextColor(colors.white)
 monitor.setCursorPos(1,3)
 monitor.write("Current Energy Level")
- 
- 
+
 -- end of init //
-print("Done. Starting Monitoring...")
-print("Press Ctrl+T to terminate the program.")
-os.sleep(2)
- 
+Console.WriteLine(Console.Type.Info, "Initialization done.", m)
+Console.WriteLine(Console.Type.Hint, "Press CTRL+T to terminate the program.", m)
+
+
 -- main program //
 while true do
   monitor.setCursorPos(1,4)
-  elv = cb.getEnergyStored()
+  energylevel = cb.getEnergyStored()
   monitor.clearLine()
   --------------------------
-  number = elv
-  function comma(number)
- 
-    if string.len(number) < 6 then
-    elseif string.len(number) > 6 then
-      number = string.gsub(number, "^(-?%d+)(%d)(%d)(%d)(%d%d%d)", '%1,%2'.." m")
-    end
-  return number
-  end
+ -- function comma(number)
+ --   if string.len(number) < 6 then
+ --   elseif string.len(number) > 6 then
+ --     number = string.gsub(number, "^(-?%d+)(%d%d%d)(%d%d%d)", '%1.%2.%3')
+ --   end
+ -- return number
+ -- end
   -----------------------
- 
-  monitor.write(comma(number).." RF")
- 
+  
+  monitor.write(numformat(energylevel).." RF")
+  
   -- RF trigger //
-  if elv < MinRF then
-     redstone.setOutput("right", true)
+  if energylevel < MinRF then
+     redstone.setOutput("left", true)
      monitor.setCursorPos(1,5)
      monitor.setTextColor(16384) --Text red
      monitor.write("Energy Backup active")
- 
-  elseif elv > MinRF then
-    redstone.setOutput("right", false)
-    monitor.setCursorPos(1,5)
-    monitor.clearLine()
+    elseif energylevel > MinRF then
+      redstone.setOutput ("right", false)
+      monitor.setCursorPos(1,5)
+      monitor.clearLine()
   end
- 
- 
+
+  
   sleep(tickcount)
- 
+  
 end
