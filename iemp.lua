827dbfcbeee39c89e4c014a94e0231401191c181
miscellaniuz <43073620+miscellaniuz@users.noreply.github.com>
2018-09-15 22:17:44 +0200

    issue #10 and #6 solved
    version 0.3c
    Changes: -Console Output now fine. -Issues 6 and 10 solved. -added few comments and formatted code for better reading and understanding.

--- iemp.lua
+++ iemp.lua
@@ -22,13 +22,13 @@ MinRF = 140000000 -- Minimum amount of RF storage before triggering energy backu
 Console.Init()
 Console.ClearScreen()
 Console.WriteLine(Console.Type.Info, "Welcome to IEMP.", m)
-Console.WriteLine(Console.Type.Line, "", m)
+Console.WriteLine(Console.Type.Line, "---------------", m)
 Console.WriteLine(Console.Type.Info, "We are running on version " .. version, m)
-Console.WriteLine(Console.Type.Line, "", m)
-Console.WriteLine(Console.Type.Init, "UtilsAPI loaded.", m)
+Console.WriteLine(Console.Type.Line, "---------------", m)
 Console.WriteLine(Console.Type.Config, "Update interval is set to " .. tickcount, m)
 Console.WriteLine(Console.Type.Config, "Minimum trigger is set to " .. MinRF, m)
-
+Console.WriteLine(Console.Type.Line, "---------------", m)
+Console.WriteLine(Console.Type.Init, "UtilsAPI loaded.", m)
 
 -- init //
 local cb = peripheral.wrap("capacitor_bank_1")
@@ -45,6 +45,7 @@ monitor.setCursorPos(1,3)
 monitor.write("Current Energy Level")
 
 -- end of init //
+Console.WriteLine(Console.Type.Line, "---------------", m)
 Console.WriteLine(Console.Type.Info, "Initialization done.", m)
 Console.WriteLine(Console.Type.Hint, "Press CTRL+T to terminate the program.", m)
 
@@ -56,18 +57,19 @@ while true do
   monitor.clearLine()
 
   number = energylevel -- avoid attempt to call nil error
-  monitor.write(numformat(number).." RF")
+  monitor.write(StringUtils.numformat(number).." RF")
   
   -- RF trigger //
   if energylevel < MinRF then
      redstone.setOutput("left", true)
+     monitor.setTextColor(colors.red) -- Text red
      monitor.setCursorPos(1,5)
-     monitor.setTextColor(16384) --Text red
      monitor.write("Energy Backup active")
     elseif energylevel > MinRF then
       redstone.setOutput ("right", false)
+      monitor.setTextColor(colors.green) -- Text back to green
       monitor.setCursorPos(1,5)
-      monitor.clearLine()
+      monitor.clearLine() -- deleting "Energy Backup active"
   end
 
   
