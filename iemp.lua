b9514f3b7bf49e1031e11e8a2eeb2209664b90fd
miscellaniuz <43073620+miscellaniuz@users.noreply.github.com>
2018-09-21 22:50:29 +0200

    rewritten code to fit changed positions of peripherals

--- iemp.lua
+++ iemp.lua
@@ -1,23 +1,21 @@
--- ////////////////////////
---
 -- Intelligent Energy Management Program (IEMP)
 -- 
--- Version: 0.3c
+-- Version: 0.4a
 --
 -- Author: Miscellaniuz
 --
 -- ////////////////////////
-
+ 
 -- @ version //
-version = "0.3c" 
-
+version = "0.4a"
+ 
 -- ToTheCore Utils import //
 os.loadAPI("/Utils/Utils")
-
+ 
 -- Variables //
-tickcount = 2 --Update interval of monitor in sec
+tickcount = 0.5 --Update interval of monitor in sec
 MinRF = 140000000 -- Minimum amount of RF storage before triggering energy backup system
-
+ 
 -- Startup
 Console.Init()
 Console.ClearScreen()
@@ -29,51 +27,75 @@ Console.WriteLine(Console.Type.Config, "Update interval is set to " .. tickcount
 Console.WriteLine(Console.Type.Config, "Minimum trigger is set to " .. MinRF, m)
 Console.WriteLine(Console.Type.Line, "---------------", m)
 Console.WriteLine(Console.Type.Init, "UtilsAPI loaded.", m)
-
+ 
 -- init //
-local cb = peripheral.wrap("capacitor_bank_1")
+local cbmain = peripheral.wrap("capacitor_bank_2")
+local cbbackup1 = peripheral.wrap("capacitor_bank_3")
+local cbbackup2 = peripheral.wrap("capacitor_bank_4")
 Console.WriteLine(Console.Type.Init, "Capacitor Bank 1 found.", m)
-local monitor = peripheral.wrap("top")
-Console.WriteLine(Console.Type.Init, "Monitor found on top.", m)
+Console.WriteLine(Console.Type.Init, "Capacitor Bank 2 found.", m)
+Console.WriteLine(Console.Type.Init, "Capacitor Bank 3 found.", m)
+ 
+ 
+local monitor = peripheral.wrap("right")
+Console.WriteLine(Console.Type.Init, "Monitor found on right.", m)
+ 
+-- Main Monitor Init //
 monitor.clear()
-monitor.setTextColor(colors.green)
 monitor.setTextScale(0.5)
+monitor.setTextColor(colors.yellow)
 -- monitor.setBackgroundColor(255) --deactivated for later
 monitor.setCursorPos(1,1)
 monitor.write("Intelligent Energy Management Program [IEMP] ver " .. version)
+monitor.setCursorPos(1,2)
+monitor.write("------------------------------------------------------")
+ 
 monitor.setTextColor(colors.white)
-monitor.setCursorPos(1,3)
+monitor.setCursorPos(1,4)
 monitor.write("Current Energy Level")
+monitor.setCursorPos(1,5)
+monitor.setTextColor(colors.blue) monitor.write("Main Capacitor Bank: ")
+monitor.setCursorPos(1,6)
+monitor.setTextColor(colors.blue) monitor.write("Backup Capacitor Bank: ")
+monitor.setTextColor(colors.green)
 
 -- end of init //
 Console.WriteLine(Console.Type.Line, "---------------", m)
 Console.WriteLine(Console.Type.Info, "Initialization done.", m)
 Console.WriteLine(Console.Type.Hint, "Press CTRL+T to terminate the program.", m)
-
-
+ 
+ 
 -- main program //
 while true do
-  monitor.setCursorPos(1,4)
-  energylevel = cb.getEnergyStored()
-  monitor.clearLine()
-
-  number = energylevel -- avoid attempt to call nil error
-  monitor.write(StringUtils.numformat(number).." RF")
   
+  energylevel = cbmain.getEnergyStored()
+  backupenergy = cbbackup1.getEnergyStored() + cbbackup2.getEnergyStored()
+
+  number = energylevel -- avoid attempt to call nil error(
+ 
+  monitor.setCursorPos(24,5)
+  monitor.write(StringUtils.numformat(number).." RF")
+ 
+  
+  number = backupenergy
+ 
+  monitor.setCursorPos(24,6)
+  monitor.write(StringUtils.numformat(number).." RF")
+ 
   -- RF trigger //
   if energylevel < MinRF then
      redstone.setOutput("left", true)
      monitor.setTextColor(colors.red) -- Text red
-     monitor.setCursorPos(1,5)
+     monitor.setCursorPos(1,7)
      monitor.write("Energy Backup active")
     elseif energylevel > MinRF then
       redstone.setOutput ("right", false)
       monitor.setTextColor(colors.green) -- Text back to green
-      monitor.setCursorPos(1,5)
+      monitor.setCursorPos(1,7)
       monitor.clearLine() -- deleting "Energy Backup active"
   end
-
-  
+ 
+ 
   sleep(tickcount)
-  
+ 
 end
